2fbf103a035b44b9c0a5f04dab08fa912bcf9ad9
ben558 <88583978+ben558@users.noreply.github.com>
2021-12-15 19:32:39 +0200

    Main
    Through here we run everything

---
+++ Main.php
@@ -0,0 +1,41 @@
+
+<?php
+    require('.\config\Database.php');
+    require('.\config\bootstrap.html');
+
+    include_once '.\WebServiceInteraction.php';
+    include_once '.\PostManager.php';
+    include_once '.\Process.php';
+    include_once '.\Post.php';
+    include_once '.\User.php';
+
+    $host = 'localhost';
+    $db_name = 'sql_inmanage';
+    $username = 'root';
+    $password = 'h@123456';
+
+    // Instantiate DB connect
+    $database = new Database($host,$db_name,$username,$password);
+    $database->connect();
+
+    $dbCheck = $database->isEmpty();
+    
+    if($dbCheck > 0)
+    {
+        if($dbCheck == 2)
+        {
+           $database->createTables();
+        }
+        $wbs = new WebServiceInteraction($database);
+        $wbs->retrieveData();
+    
+        $process = new Process($database);
+        $process->associateAlbums();
+        $process->associateComments();
+    }
+
+    $pManager = new PostManager($database);
+
+    print_r($pManager->retrieveFromTable());
+
+?>
